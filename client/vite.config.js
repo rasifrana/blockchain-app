63ecad7c9471be4d041af9cfc3df805b49f9a909
Asif Rana <muhammadasif@MUHAMMADS-MBP.station>
2022-03-10 17:50:44 +0000

    adding changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://rasifrana.github.io/blockchain-app',
+  base: '/blockchain-app/',
   plugins: [react()]
 })
